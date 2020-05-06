9237a091ac1333eba9d0c48c7c7d1bfb0875081d
Souhail Maraoui <m.souhail2@outlook.com>
2020-05-06 16:04:05 +0000

    Initial commit

---
+++ src/main/java/com/aseds/costats/CoStatsApplication.java
@@ -0,0 +1,13 @@
+package com.aseds.costats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aseds/costats/CoStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aseds.costats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
